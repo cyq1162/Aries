c43bc86bde0b44bf4b0a967aed94be3ec666149a
Aster <272657997@qq.com>
2016-09-12 15:19:26 +0800

    去掉index.html多出来的z字母。

--- Aries.DevFramework/Web.UI/Style/JS/Aries.Loader.js
+++ Aries.DevFramework/Web.UI/Style/JS/Aries.Loader.js
@@ -3,7 +3,6 @@
 /// <reference path="/Style/JS/AriesJS/Aries.DataGrid.js" />
 /// <reference path="/Style/JS/AriesJS/Aries.Form.js" />
 /// <reference path="/Style/JS/AriesJS/Aries.EasyUIExtend.js" />
-/// <reference path="/Style/JS/AriesJS/Aries.DataGrid.js" />
 /// <reference path="/Style/JS/AriesJS/Aries.Common.js" />
 /// <reference path="json2.js" />
 /// <reference path="/Style/JS/AriesJS/Aries.Config.js" />
@@ -22,22 +21,23 @@ function getCookie(name) {
     return "";
 }
 var ui = getCookie('sys_ui');
+var vs = "v3.100";
 document.write('<link id="lk_theme" href="'+ui+'/Style/JS/EasyUI-1.3.4/themes/default/easyui.css" rel="stylesheet" type="text/css" />');
 document.write('<link href="' + ui + '/Style/CSS/main.css" rel="stylesheet" />');
 document.write('<link href="' + ui + '/Style/JS/EasyUI-1.3.4/themes/icon.css" rel="stylesheet" type="text/css" />');
 
 document.write('<script src="' + ui + '/Style/JS/jquery-1.7.1.min.js"></script>');
 document.write('<script src="' + ui + '/Style/JS/json2.js" ></script>');
-document.write('<script src="' + ui + '/Style/JS/EasyUI-1.3.4/jquery.easyui.min.js"></script>');
+document.write('<script src="' + ui + '/Style/JS/EasyUI-1.3.4/jquery.easyui.min.js?"></script>');
 document.write('<script src="' + ui + '/Style/JS/EasyUI-1.3.4/locale/easyui-lang-zh_CN.js"></script>');
-document.write('<script src="' + ui + '/Style/JS/AriesJS/Aries.EasyUIExtend.js"></script>');
-document.write('<script src="' + ui + '/Style/JS/AriesJS/Aries.Utility.js"></script>');
-document.write('<script src="' + ui + '/Style/JS/AriesJS/Aries.Upload.js"></script> ');
-document.write('<script src="' + ui + '/Style/JS/AriesJS/Aries.Common.js"></script>');
-document.write('<script src="' + ui + '/Style/JS/AriesJS/Aries.DataGrid.js"></script>');
-document.write('<script src="' + ui + '/Style/JS/AriesJS/Aries.Form.js?r=1"></script>');
-document.write('<script src="' + ui + '/Style/JS/AriesJS/Aries.Config.js"></script>');
-document.write('<script src="' + ui + '/Style/JS/AriesJS/Aries.Themes.js"></script>');
+document.write('<script src="' + ui + '/Style/JS/AriesJS/Aries.EasyUIExtend.js?v=' + vs + '"></script>');
+document.write('<script src="' + ui + '/Style/JS/AriesJS/Aries.Utility.js?v=' + vs + '"></script>');
+document.write('<script src="' + ui + '/Style/JS/AriesJS/Aries.Upload.js?v=' + vs + '"></script> ');
+document.write('<script src="' + ui + '/Style/JS/AriesJS/Aries.Common.js?v=' + vs + '"></script>');
+document.write('<script src="' + ui + '/Style/JS/AriesJS/Aries.DataGrid.js?v=' + vs + '"></script>');
+document.write('<script src="' + ui + '/Style/JS/AriesJS/Aries.Form.js?v=' + vs + '"></script>');
+document.write('<script src="' + ui + '/Style/JS/AriesJS/Aries.Config.js?v=' + vs + '"></script>');
+document.write('<script src="' + ui + '/Style/JS/AriesJS/Aries.Themes.js?v=' + vs + '"></script>');
 
 
 

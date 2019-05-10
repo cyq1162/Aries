728784a5d8ccc023385a4dd12c010f90f4fe4b47
Aster <272657997@qq.com>
2019-05-10 17:55:45 +0800

    V5.4 2019.4 -2019.5 框架大调整、同时新增 Gemini.Workflow 支持 1、跨库功能由 CYQ.Data 集成、并去掉了Aries自带的跨库功能。 2、变量提升等级： AR.Global.Dialog => AR.Dialog AR.Utility.Window => AR.Window AR.Utility.Cookie => AR.Cookie AR.Utiltity.Ajax => AR.Ajax 3、内部细节和性能优化（其它大体调用方式没变）

--- Aries.DevFramework/Web.UI/Properties/AssemblyInfo.cs
+++ Aries.DevFramework/Web.UI/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 // 则将该类型上的 ComVisible 特性设置为 true。
 [assembly: ComVisible(false)]
 
-// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 id
 [assembly: Guid("6855fdba-cf7c-4468-9c2e-8bea63eac5a0")]
 
 // 程序集的版本信息由下列四个值组成:

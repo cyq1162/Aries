226341bee9439ea96f0996b58a0591d7380e0223
Aster <272657997@qq.com>
2021-10-18 14:57:12 +0800

    优化NETCore版本：工作流。

--- Aries.DevFramework/Web.UI_NetCore/Program.cs
+++ Aries.DevFramework/Web.UI_NetCore/Program.cs
@@ -1,32 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using CYQ.Data;
-
 namespace Web.UI_NetCore
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            IHostBuilder hostBuilder = CreateHostBuilder(args);
+            IHost host = hostBuilder.Build();
+            host.Run();
+        }
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            IHostBuilder hostBuilder = Host.CreateDefaultBuilder(args);
+            hostBuilder.ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+                webBuilder.ConfigureKestrel((context, options) =>
+                {
+                    //设置应用服务器Kestrel请求体最大为50MB
+                    options.Limits.MaxRequestBodySize = null;
+                });
+                //webBuilder.UseIIS();
+            });
+            return hostBuilder;
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-               // .UseUrls(GetUrl())
-                .Build();
-        public static string GetUrl()
-        {
-            string url = AppConfig.GetApp("Host", "[http|https]://*:6666");
-            return url;
-        }
+        public static IHostBuilder CreateHostBuilder2(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureKestrel((context, options) =>
+                    {
+                        //设置应用服务器Kestrel请求体最大为50MB
+                        options.Limits.MaxRequestBodySize = null;
+                    });
+                    //webBuilder.UseIIS();
+                });
     }
 }
